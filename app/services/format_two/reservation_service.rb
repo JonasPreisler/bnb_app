732a3631030c6a9106b7515760f287171862c0f2
Jonas <jonas.preisler@g>
2021-03-25 00:52:14 +0800

    fixed params for payload 2

--- app/services/format_two/reservation_service.rb
+++ app/services/format_two/reservation_service.rb
@@ -11,17 +11,16 @@ class FormatTwo::ReservationService
                                       guest_id:            @guest.id,
                                       start_date:          @params[:start_date],
                                       end_date:            @params[:end_date],
-                                      payout_amount:       @params[:expected_payout_amount],
+                                      payout_amount:       @params[:payout_price],
                                       currency:            @params[:currency],
                                       nights:              @params[:nights],
-                                      number_of_guests:    @params[:number_of_guests],
+                                      number_of_guests:    @params[:guests],
                                       status:              @params[:status],
-                                      total_paid_amount:   @params[:total_paid_amount_accurate],
                                       adults:              @params[:adults],
                                       children:            @params[:children],
                                       infants:             @params[:infants],
-                                      security_amount:     @params[:listing_security_price_accurate],
-                                      total_amount:        @params[:total_paid_amount_accurate]
+                                      security_amount:     @params[:security_price],
+                                      total_amount:        @params[:total_price]
                                     )
 
       reservation.save!
